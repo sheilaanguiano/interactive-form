5c95a0d062be2f9d7fbc2a5caa6ed252e589d2b8
Sheila Anguiano <sheila.anguiano@gmail.com>
2022-06-08 23:29:21 -0700

    activities event listener

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const jobTitle = document.querySelector('#title');
 const otherJob = document.querySelector('#other-job-role');
 const shirtDesign = document.querySelector('#design');
 const shirtColor = document.querySelector('#color');
+const activities = document.querySelector('#activities');
+const activitiesCost = document.querySelector('#activities-cost');
+const checkboxes = document.querySelectorAll('.activities input');
+
 
 nameField.focus();
 otherJob.style.display = 'none';
@@ -22,12 +26,28 @@ shirtDesign.addEventListener('change', e => {
     shirtColor.disabled = false;
 
     for(let i = 0; i < shirtColor.length; i++){
+        
         if(e.target.value !== shirtColor.children[i].getAttribute('data-theme')){
             shirtColor.children[i].style.display = 'none';
         } else {
             shirtColor.children[i].style.display = 'initial'
         } 
     }
+});
+
+/*
+    ACTIVITIES EVENT LISTENER
+*/ 
+let totalCost= 0;
+
+activities.addEventListener('change', e => {
+    let clicked = e.target
+    let eventCost = parseInt(clicked.getAttribute('data-cost'));
+
+    totalCost = clicked.checked ? totalCost + eventCost
+                                :  totalCost - eventCost;
+    activitiesCost.innerHTML= `Total $${totalCost}`;
+      
 });
 
 
